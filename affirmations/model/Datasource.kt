3095768a6dfe1ed4e93ed4ba49f2fd6a36dd9365
pawan prajapati <122220736+P1mak@users.noreply.github.com>
2023-07-25 12:19:56 +0530

    Added affirmations

---
+++ affirmations/model/Datasource.kt
@@ -0,0 +1,39 @@
+/*package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+class Datasource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.hello, R.drawable.im),
+            Affirmation(R.string.how_are_you, R.drawable.km),
+            Affirmation(R.string.hii, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km),
+            Affirmation(R.string.affirmation1, R.drawable.km))
+    }
+}*/
+
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+class Datasource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.how_are_you, R.drawable.image2),
+            Affirmation(R.string.hii, R.drawable.image3),
+            Affirmation(R.string.hello, R.drawable.image4),
+            Affirmation(R.string.its_a_great_day, R.drawable.image5),
+            Affirmation(R.string.i_am_fine, R.drawable.image6),
+            Affirmation(R.string.be_greatfull, R.drawable.image7),
+            Affirmation(R.string.have_faith, R.drawable.image8),
+            Affirmation(R.string.Good_to_see_you, R.drawable.image9),
+            Affirmation(R.string.Again_Come, R.drawable.image10))
+    }
+}
